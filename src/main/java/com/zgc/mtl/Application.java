f9354039ae1591f883e6e8e3dd79f5f8a9de5363
developer1001 <yangpeng_188@sina.com>
2019-06-18 15:00:43 +0800

    健康模块

--- src/main/java/com/zgc/mtl/Application.java
+++ src/main/java/com/zgc/mtl/Application.java
@@ -3,7 +3,6 @@ package com.zgc.mtl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 /**
  * 
---
+++ src/main/java/com/zgc/mtl/controller/HealthyController.java
@@ -0,0 +1,22 @@
+package com.zgc.mtl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zgc.mtl.base.model.Json;
+
+/**
+ * 
+ * @date 2019-06-18 11:32:16
+ * @author yang
+ */
+@RequestMapping("healthy")
+public class HealthyController {
+	
+	@RequestMapping("bgm")
+	@ResponseBody
+	public Json bgm() {
+		
+		return null;
+	}
+}
