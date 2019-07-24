41e55ace00e75e5182d67e42553b58714a24a093
developer1001 <yangpeng_188@sina.com>
2019-07-24 16:06:55 +0800

    更新数据库备份文件

--- src/main/resources/sql/mtl.sql
+++ src/main/resources/sql/mtl.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : zs
-Source Server Version : 50717
+Source Server         : localhost
+Source Server Version : 50522
 Source Host           : localhost:3306
 Source Database       : mtl
 
 Target Server Type    : MYSQL
-Target Server Version : 50717
+Target Server Version : 50522
 File Encoding         : 65001
 
-Date: 2018-07-31 17:09:27
+Date: 2019-07-24 15:56:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -39,6 +39,29 @@ INSERT INTO `sys_user` VALUES ('5', '企业1', 'qiye1', 'e10adc3949ba59abbe56e05
 INSERT INTO `sys_user` VALUES ('7', '测试改', 'ceshi11', 'e10adc3949ba59abbe56e057f20f883e', '0');
 
 -- ----------------------------
+-- Table structure for t_healthy
+-- ----------------------------
+DROP TABLE IF EXISTS `t_healthy`;
+CREATE TABLE `t_healthy` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `level` double(10,3) DEFAULT NULL COMMENT '指数',
+  `level_type` int(2) DEFAULT NULL COMMENT '1:bg（血糖）; 2:weight; 3:bp（血压）',
+  `user_id` varchar(20) DEFAULT NULL COMMENT '用户编号',
+  `create_date` datetime DEFAULT NULL COMMENT '创建日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='健康指数表';
+
+-- ----------------------------
+-- Records of t_healthy
+-- ----------------------------
+INSERT INTO `t_healthy` VALUES ('3', '3.000', '4', '1', '2019-07-20 11:20:45');
+INSERT INTO `t_healthy` VALUES ('4', '2.200', '4', '1', '2019-07-24 11:20:45');
+INSERT INTO `t_healthy` VALUES ('5', '6.000', '4', '1', '2019-07-22 11:20:45');
+INSERT INTO `t_healthy` VALUES ('6', '3.000', '4', '5', '2019-07-24 11:20:45');
+INSERT INTO `t_healthy` VALUES ('7', '3.000', '4', '5', '2019-07-24 11:20:45');
+INSERT INTO `t_healthy` VALUES ('8', '3.000', '4', '5', '2019-07-24 11:20:45');
+
+-- ----------------------------
 -- Table structure for t_person
 -- ----------------------------
 DROP TABLE IF EXISTS `t_person`;
@@ -46,11 +69,14 @@ CREATE TABLE `t_person` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `age` int(3) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `idx_name_age` (`name`,`age`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_person
 -- ----------------------------
 INSERT INTO `t_person` VALUES ('1', '一号', '25');
-INSERT INTO `t_person` VALUES ('2', '二号', '26');
+INSERT INTO `t_person` VALUES ('3', '三号', '24');
+INSERT INTO `t_person` VALUES ('2', '二号', '23');
+INSERT INTO `t_person` VALUES ('4', '四号', '26');
