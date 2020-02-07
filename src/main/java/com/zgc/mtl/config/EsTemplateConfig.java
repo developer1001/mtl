53c1408a76ab74f1c2f0a85799ac366bb239d342
developer1001 <yangpeng_188@sina.com>
2020-02-07 16:33:43 +0800

    import package

--- src/main/java/com/zgc/mtl/config/EsTemplateConfig.java
+++ src/main/java/com/zgc/mtl/config/EsTemplateConfig.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-
-import com.zgc.mtl.model.Product;
 /**
  * 主要用来构建RestHighLevelClient客户端
  * @date 2019-07-12 16:47:15
